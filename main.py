db1fac5dfb5a835cfebcfead088ee3d2e8897478
Rajeev S <raghu2raj@gmail.com>
2021-08-13 19:17:27 +0530

    add Flask app

--- main.py
+++ main.py
@@ -1,43 +1,24 @@
-"""Translation App"""
+"""Translation App Endpoints."""
 
-from ibm_watson import ApiException
-
-from translation_service import LANGUAGE_TRANSLATOR
-
-
-def english_to_french(english_text: str) -> str:
-    """Translate from English to French
-
-    Args:
-        english_text (str): english text being translated
-
-    Returns:
-        str: translated french string
-    """
-    try:
-        lang_response = LANGUAGE_TRANSLATOR.translate(
-            text=english_text, model_id="en-fr"
-        )
-        resp = lang_response.get_result()
-        return resp["translations"][0]["translation"]
-    except ApiException as api_err:
-        return (api_err.code, api_err.message)
+# import translation_app
+import translation_service
+from flask import Flask, request
 
 
-def french_to_english(french_text: str) -> str:
-    """French to English Translation
+app = Flask("Language Translation")
 
-    Args:
-        french_text (str): French
 
-    Returns:
-        str: English
-    """
-    try:
-        lang_response = LANGUAGE_TRANSLATOR.translate(
-            text=french_text, model_id="fr-en"
-        )
-        resp = lang_response.get_result()
-        return resp["translations"][0]["translation"]
-    except ApiException as api_err:
-        return (api_err.code, api_err.message)
+@app.route("/eng2fr", methods=["POST"])
+def translate_eng2fr():
+    """Translate English to French by calling translation service API"""
+    text = request.form["text"]
+    result = translation_service.english_to_french(text)
+    return {"text": text, "translated": result}
+
+
+@app.route("/fr2eng", methods=["POST"])
+def translate_fr2eng():
+    """Translate French to English by calling translation service API"""
+    text = request.form["text"]
+    result = translation_service.french_to_english(text)
+    return {"text": text, "translated": result}
